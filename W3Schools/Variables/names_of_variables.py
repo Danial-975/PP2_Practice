f27bf6083f3d49c4893efdf4c047277cbcfd2094
Danial-975 <zil.ninzya@gmail.com>
2026-01-30 01:34:31 +0500

    Names of Variables

---
+++ W3Schools/Variables/names_of_variables.py
@@ -0,0 +1,14 @@
+myname = "Danial"
+my_friend = "Alim"
+_my_friend = "Aizere"
+myFriend = "Zhaisan"
+MYFRIEND = "Elzhas"
+myfriend2 = "Nurbek"
+
+
+print(myname)
+print(my_friend)
+print(_my_friend)
+print(myFriend)
+print(MYFRIEND)
+print(myfriend2)
