1c63bb7603a209339ecc506cf8ac66203b0c2bab
Danial-975 <zil.ninzya@gmail.com>
2026-02-06 00:31:27 +0500

    Switch/If Elif/Else

---
+++ W3Schools/If_Else/match4.py
@@ -0,0 +1,10 @@
+month = 9
+match month:
+  case 12 | 1 | 2 :
+    print("It is winter")
+  case 3 | 4 | 5 : 
+    print("It is spring")
+  case 6 | 7 | 8 : 
+    print("It is summer")
+  case 9 | 10 | 11 : 
+    print("It is autumn")
