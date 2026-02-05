f802d22a4e3eb6906eafa618da8fd6796ac8468e
Danial-975 <zil.ninzya@gmail.com>
2026-02-05 23:56:38 +0500

    If Else

---
+++ W3Schools/If_Else/ifElse1.py
@@ -0,0 +1,6 @@
+number = 9
+
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
---
+++ W3Schools/If_Else/ifElse2.py
@@ -0,0 +1,6 @@
+user = "Danial"
+
+if len(user) > 0:
+  print(f"Welcome, {user}!")
+else:
+  print("Error: User cannot be empty")
