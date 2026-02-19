046074e285794e808bd9a2ebb8488ca94ce2aae3
Danial-975 <zil.ninzya@gmail.com>
2026-02-20 00:28:40 +0500

    Using lambda with sorted() for custom sorting

---
+++ W3Schools/Practice3/Lambda/lambda_sorted/sorted2.py
@@ -0,0 +1,3 @@
+students = [("Aituar", 18), ("Elzhas", 17), ("Alim", 18)]
+sorted_students = sorted(students, key=lambda x: x[1])
+print(sorted_students)
---
+++ W3Schools/Practice3/Lambda/lambda_sorted/sorted3.py
@@ -0,0 +1,3 @@
+students = [("Aituar", 18), ("Elzhas", 17), ("Alim", 18)]
+sorted_students = sorted(students, key=lambda x: x[1] , reverse=True)
+print(sorted_students)
