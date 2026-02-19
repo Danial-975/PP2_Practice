87c52d33ccebc84b016164a9583f6f81e64a7163
Danial-975 <zil.ninzya@gmail.com>
2026-02-20 00:44:19 +0500

    Anonymous functions vs regular functions

---
+++ W3Schools/Practice3/Lambda/Anonymous_and_regular_functions/def.py
@@ -0,0 +1,10 @@
+def myfunc(age):
+    if age<13:
+        print("You are a kid")
+    elif age<19:
+        print("You are a teenager")
+    elif age<64:
+        print("You are an adult")
+    else:
+        print("You are a senior")
+myfunc(20)
