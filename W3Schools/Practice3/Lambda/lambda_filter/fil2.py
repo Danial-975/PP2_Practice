ccfa728d0f356880df926e44e08a3b3f9627a485
Danial-975 <zil.ninzya@gmail.com>
2026-02-20 00:21:08 +0500

    Using lambda with filter() for selection

---
+++ W3Schools/Practice3/Lambda/lambda_filter/fil2.py
@@ -0,0 +1,3 @@
+numbers = [-1, 2, 4, -4, 11, 6, -7, 9]
+positive_numbers = list(filter(lambda x: x > 0, numbers))
+print(positive_numbers)
---
+++ W3Schools/Practice3/Lambda/lambda_filter/fil3.py
@@ -0,0 +1,3 @@
+numbers = [-1, 2, 4, -4, 11, 6, -7, -9]
+negative_numbers = list(filter(lambda x: x < 0, numbers))
+print(negative_numbers)
