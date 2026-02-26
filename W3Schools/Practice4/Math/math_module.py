5e0a48ca934c3a553b569ac9534a2a279b76c6ee
Danial-975 <zil.ninzya@gmail.com>
2026-02-27 00:29:18 +0500

    Learn Python Math

---
+++ W3Schools/Practice4/Math/math_module.py
@@ -0,0 +1,13 @@
+import math
+
+x = math.sqrt(64)
+y = math.ceil(1.4)
+z = math.floor(1.4)
+a = math.pi
+b = math.sin(math.pi/6)
+
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
---
+++ W3Schools/Practice4/Math/random_module.py
@@ -0,0 +1,9 @@
+import random
+
+mylist = ["apple", "banana", "cherry"]
+
+random.shuffle(mylist)
+print(mylist)
+print(random.choice(mylist))
+print(random.randint(3, 9))
+print(random.random())
